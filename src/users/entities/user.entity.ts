b8d0737202081dca8ed83001a8455e0ec8bfc18f
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-10-03 22:11:18 -0300

    chore: add db connect

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ name: 'razao_social', type: 'varchar' })
+  username: string;
+
+  @Column({ name: 'razao_social', type: 'varchar' })
+  password: string;
+
+  @Column({
+    name: 'criado_em',
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'atualizado_em',
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+}
